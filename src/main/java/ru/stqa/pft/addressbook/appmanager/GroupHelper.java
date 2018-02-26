6ac6ae2aec456bd39b4696c7a82cbe51961da76c
vladimirov <denis.vladimiroff@gmail.com>
2018-02-27 00:26:44 +0200

    added lesson 40

--- src/main/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
+++ src/main/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
@@ -55,4 +55,8 @@ public class GroupHelper extends HelperBase {
     public boolean isThereAGroup() {
         return isElementPresent(By.name("selected[]"));
     }
+
+    public int getGroupCount() {
+        return driver.findElements(By.name("selected[]")).size();
+    }
 }
