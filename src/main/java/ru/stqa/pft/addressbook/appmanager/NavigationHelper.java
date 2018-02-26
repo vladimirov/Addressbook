5a12d4d57516e221d0298fd620fe0194218f9595
vladimirov <denis.vladimiroff@gmail.com>
2018-02-26 12:44:08 +0200

    added lesson 35

--- src/main/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
+++ src/main/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
@@ -14,6 +14,6 @@ public class NavigationHelper extends HelperBase{
     }
 
     public void gotoHomePage() {
-        driver.get("http://localhost/addressbook");
+        click(By.linkText("home"));
     }
 }
