9df4e34319bf5139633ab037075c36ff0ce7f46e
vladimirov <denis.vladimiroff@gmail.com>
2018-02-26 12:58:17 +0200

    optimized page transitions lesson 36

--- src/main/java/ru/stqa/pft/addressbook/appmanager/HelperBase.java
+++ src/main/java/ru/stqa/pft/addressbook/appmanager/HelperBase.java
@@ -6,42 +6,42 @@ import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 
 public class HelperBase {
-    protected WebDriver driver;
+  protected WebDriver driver;
 
-    public HelperBase(WebDriver driver) {
-        this.driver = driver;
-    }
+  public HelperBase(WebDriver driver) {
+    this.driver = driver;
+  }
 
-    protected void click(By locator) {
-        driver.findElement(locator).click();
-    }
+  protected void click(By locator) {
+    driver.findElement(locator).click();
+  }
 
-    protected void type(By locator, String text) {
-        click(locator);
-        if (text != null) {
-            String existingText = driver.findElement(locator).getAttribute("value");
-            if (!text.equals(existingText)) {
-                driver.findElement(locator).clear();
-                driver.findElement(locator).sendKeys(text);
-            }
-        }
+  protected void type(By locator, String text) {
+    click(locator);
+    if (text != null) {
+      String existingText = driver.findElement(locator).getAttribute("value");
+      if (!text.equals(existingText)) {
+        driver.findElement(locator).clear();
+        driver.findElement(locator).sendKeys(text);
+      }
     }
+  }
 
-    public boolean isAlertPresent() {
-        try {
-            driver.switchTo().alert();
-            return true;
-        } catch (NoAlertPresentException e) {
-            return false;
-        }
+  public boolean isAlertPresent() {
+    try {
+      driver.switchTo().alert();
+      return true;
+    } catch (NoAlertPresentException e) {
+      return false;
     }
+  }
 
-    protected boolean isElementPresent(By locator) {
-        try {
-            driver.findElement(locator);
-            return true;
-        } catch (NoSuchElementException e) {
-            return false;
-        }
+  protected boolean isElementPresent(By locator) {
+    try {
+      driver.findElement(locator);
+      return true;
+    } catch (NoSuchElementException e) {
+      return false;
     }
+  }
 }
--- src/main/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
+++ src/main/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
@@ -3,17 +3,25 @@ package ru.stqa.pft.addressbook.appmanager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
-public class NavigationHelper extends HelperBase{
+public class NavigationHelper extends HelperBase {
 
-    public NavigationHelper(WebDriver driver) {
-        super(driver);
-    }
+  public NavigationHelper(WebDriver driver) {
+    super(driver);
+  }
 
-    public void goToGroupPage() {
-        click(By.linkText("groups"));
+  public void goToGroupPage() {
+    if (isElementPresent(By.tagName("h1"))
+            && driver.findElement(By.tagName("h1")).getText().equals("Groups")
+            && isElementPresent(By.name("new"))) {
+      return;
     }
+      click(By.linkText("groups"));
+  }
 
-    public void gotoHomePage() {
-        click(By.linkText("home"));
+  public void gotoHomePage() {
+    if (isElementPresent(By.id("maintable"))){
+      return;
     }
+    click(By.linkText("home"));
+  }
 }
