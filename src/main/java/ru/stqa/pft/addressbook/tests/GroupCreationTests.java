1768e8ff484e71c946000e8c71674a4388b98e99
vladimirov <denis.vladimiroff@gmail.com>
2018-02-28 12:09:28 +0200

    added lesson 45

--- src/main/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ src/main/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -5,6 +5,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 
@@ -14,17 +15,19 @@ public class GroupCreationTests extends TestBase {
     public void testGroupCreation() {
         app.getNavigationHelper().goToGroupPage();
         List<GroupData> before = app.getGroupHelper().getGroupList();
-        GroupData group = new GroupData("test1", null, null);
+        GroupData group = new GroupData("test2", null, null);
         app.getGroupHelper().createGroup(group);
         List<GroupData> after = app.getGroupHelper().getGroupList();
         Assert.assertEquals(after.size(), before.size() + 1);
-
-        int max = 0;
-        for (GroupData g : after) {
-            if (g.getId() > max) {
-                max = g.getId();
-            }
-        }
+        //Вычисление максимального значения 'value' у группы
+//        int max = 0;
+//        for (GroupData g : after) {
+//            if (g.getId() > max) {
+//                max = g.getId();
+//            }
+//        }
+        //Вычисление максимального значения 'value' у группы с помощью люмбда выражений
+        int max = after.stream().max(Comparator.comparingInt(GroupData::getId)).get().getId();
         group.setId(max);
         before.add(group);
         Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
