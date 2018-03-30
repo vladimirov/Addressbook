6501097990551a4a208e9dbddf148c788fd0a206
vladimirov <denis.vladimiroff@gmail.com>
2018-03-30 16:56:06 +0300

    lesson 65 - format JSON

--- src/main/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ src/main/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -1,6 +1,8 @@
 package ru.stqa.pft.addressbook.tests;
 
 
+import com.google.common.reflect.TypeToken;
+import com.google.gson.Gson;
 import com.thoughtworks.xstream.XStream;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
@@ -19,23 +21,22 @@ import static org.hamcrest.MatcherAssert.assertThat;
 public class GroupCreationTests extends TestBase {
 
     @DataProvider
-    public Iterator<Object[]> validGroups() throws IOException {
-        BufferedReader reader = new BufferedReader(new FileReader(new File("src/main/resources/groups.xml")));
-        String xml = "";
+    public Iterator<Object[]> validGroupsFromJson() throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader(new File("src/main/resources/groups.json")));
+        String json = "";
         String line = reader.readLine();
         while (line != null) {
 //            String[] split = line.split(";");
 //            list.add(new Object[]{new GroupData().withName(split[0]).withHeader(split[1]).withFooter(split[2])});
-            xml += line;
+            json += line;
             line = reader.readLine();
         }
-        XStream xStream = new XStream();
-        xStream.processAnnotations(GroupData.class);
-        List<GroupData> groups = (List<GroupData>) xStream.fromXML(xml);
+        Gson gson = new Gson();
+        List<GroupData> groups = gson.fromJson(json, new TypeToken<List<GroupData>>(){}.getType());
         return groups.stream().map((g) -> new Object[] {g}).collect(Collectors.toList()).iterator();
     }
 
-    @Test(dataProvider = "validGroups")
+    @Test(dataProvider = "validGroupsFromJson")
     public void testGroupCreation(GroupData group) {
         app.goTo().groupPage();
         Groups before = app.group().all();
