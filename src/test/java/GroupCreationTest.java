858358bee2c281bfd1fdf6d05708005c73b1a8cb
vladimirov <denis.vladimiroff@gmail.com>
2018-04-02 17:47:09 +0300

    lesson 69 - logging

--- src/test/java/GroupCreationTest.java
+++ src/test/java/GroupCreationTest.java
@@ -1,16 +1,17 @@
-
-
-
 import com.google.common.reflect.TypeToken;
 import com.google.gson.Gson;
 import com.thoughtworks.xstream.XStream;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 import ru.stqa.pft.addressbook.model.Groups;
 
-import java.io.*;
-import java.util.ArrayList;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.Iterator;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -19,6 +20,7 @@ import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class GroupCreationTest extends TestBase {
+
 
     @DataProvider
     public Iterator<Object[]> validGroupsFromXml() throws IOException {
--- src/test/java/TestBase.java
+++ src/test/java/TestBase.java
@@ -1,25 +1,43 @@
 
 
 import org.openqa.selenium.remote.BrowserType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 import ru.stqa.pft.addressbook.appmanager.ApplicationManager;
 
+import java.lang.reflect.Member;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+
 public class TestBase {
 
-//    protected static final ApplicationManager app = new ApplicationManager(BrowserType.CHROME);
-    protected static final ApplicationManager app = new ApplicationManager(System.getProperty("browser"));
+    Logger logger = LoggerFactory.getLogger(TestBase.class);
+
+
+    protected static final ApplicationManager app = new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
 
     @BeforeSuite
     public void setUp(){
         app.init();
     }
 
-    @AfterSuite
+    @AfterSuite(alwaysRun = true)
     public void tearDown() {
         app.stop();
+    }
+
+    @BeforeMethod
+    public void logTestStart(Method m, Object[] p) {
+        logger.info("Start test " + m.getName() + " with parameters " + Arrays.asList(p));
+    }
+
+    @AfterMethod
+    public void logTestStop(Method m) {
+        logger.info("Stop test " + m.getName());
     }
 
 }
