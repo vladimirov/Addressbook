cd1db2d2970c1dee96f982ac57ed4b35a3298139
vladimirov <denis.vladimiroff@gmail.com>
2018-04-02 12:38:37 +0300

    lesson 67 - added testng.xml

--- src/test/java/TestBase.java
+++ src/test/java/TestBase.java
@@ -9,7 +9,8 @@ import ru.stqa.pft.addressbook.appmanager.ApplicationManager;
 
 public class TestBase {
 
-    protected static final ApplicationManager app = new ApplicationManager(BrowserType.CHROME);
+//    protected static final ApplicationManager app = new ApplicationManager(BrowserType.CHROME);
+    protected static final ApplicationManager app = new ApplicationManager(System.getProperty("browser"));
 
     @BeforeSuite
     public void setUp(){
