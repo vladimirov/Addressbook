4f023c112e3d1c059501eebf119bd7a752f441f6
vladimirov <denis.vladimiroff@gmail.com>
2018-04-18 12:06:29 +0300

    lesson 72 - data selection conditions

--- src/main/java/ru/stqa/pft/addressbook/model/GroupData.java
+++ src/main/java/ru/stqa/pft/addressbook/model/GroupData.java
@@ -5,10 +5,10 @@ import com.thoughtworks.xstream.annotations.XStreamAlias;
 import com.thoughtworks.xstream.annotations.XStreamOmitField;
 import org.hibernate.annotations.Type;
 
-import javax.persistence.*;
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.ManyToMany;
+import javax.persistence.Id;
+import javax.persistence.Table;
 import java.util.Objects;
 
 @XStreamAlias("group")
@@ -84,13 +84,14 @@ public class GroupData {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         GroupData groupData = (GroupData) o;
-        return Objects.equals(name, groupData.name);
+        return id == groupData.id &&
+                Objects.equals(name, groupData.name);
     }
 
     @Override
     public int hashCode() {
 
-        return Objects.hash(name);
+        return Objects.hash(id, name);
     }
 }
 
--- src/test/java/GroupModificationTest.java
+++ src/test/java/GroupModificationTest.java
@@ -12,7 +12,7 @@ public class GroupModificationTest extends TestBase {
     public void ensurePreconditions() {
         app.goTo().groupPage();
         if (app.group().all().size() == 0) {
-            app.group().create(new GroupData().withName("test1"));
+            app.group().create(new GroupData().withName("test_for_mod"));
         }
     }
 
@@ -21,7 +21,7 @@ public class GroupModificationTest extends TestBase {
         Groups before = app.group().all();
         GroupData modifiedGroup = before.iterator().next();
         GroupData group = new GroupData()
-                .withId(modifiedGroup.getId()).withName("test1").withHeader("test2").withFooter("test3");
+                .withId(modifiedGroup.getId()).withName("test_mod").withHeader("header_mod").withFooter("footer_mod");
         app.goTo().groupPage();
         app.group().modify(group);
         assertThat(app.group().count(), equalTo(before.size()));//hashing and pre-validation
